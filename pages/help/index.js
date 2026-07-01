17ca67b45bc278f0d46b9eed2a8cb8e1b3393ef5
吴氏集团 <admin@wushi.com>
2026-07-01 11:17:04 +0800

    style: align help page nav bar back button with system capsule

--- pages/help/index.js
+++ pages/help/index.js
@@ -3,6 +3,9 @@ const common = require('../../utils/common.js');
 Page({
   data: {
     statusBarHeight: 0,
+    navHeight: 0,
+    capsuleTop: 0,
+    capsuleHeight: 32,
     openIndex: -1,
     categories: [
       { id: 'order',  name: '订单问题',  icon: '/assets/icons/icon-b64-13.svg' },
@@ -23,8 +26,17 @@ Page({
   },
 
   onLoad() {
-    const { statusBarHeight } = common.getNavigationHeight();
-    this.setData({ statusBarHeight });
+    const { statusBarHeight, navHeight } = common.getNavigationHeight();
+    let capsuleTop = statusBarHeight + 8;
+    let capsuleHeight = 32;
+    try {
+      const menuButton = wx.getMenuButtonBoundingClientRect();
+      if (menuButton) {
+        capsuleTop = menuButton.top;
+        capsuleHeight = menuButton.height;
+      }
+    } catch (e) {}
+    this.setData({ statusBarHeight, navHeight, capsuleTop, capsuleHeight });
   },
 
   onFaqTap(e) {
