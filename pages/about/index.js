48eb4e6d351021b78bc0302ec29807150006f538
吴氏集团 <admin@wushi.com>
2026-08-13 13:45:11 +0800

    fix(about): improve footer layout and intro fallback text; remove hardcoded linebreak in copyright

--- pages/about/index.js
+++ pages/about/index.js
@@ -23,7 +23,7 @@ Page({
     api.getAbout().catch(() => null).then((about) => {
       if (about && typeof about === 'object') {
         const intro = about.image
-          ? { title: about.intro || '', image: api.resolveImage(about.image) }
+          ? { title: about.intro || '蓉城企服是成都本土企业服务一站式平台，致力于为广大企业主及个人用户提供便捷、高效、专业的证件办理、印章刻制、公告刊登等服务。', image: api.resolveImage(about.image) }
           : null;
         this.setData({
           appName: about.appName || this.data.appName,
